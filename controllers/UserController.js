2e66fdde07a1a849ecf029d59288956e9a12f3d1
ascenciodev <alessandroascencio@protonmail.com>
2020-12-28 13:29:33 -0300

    create user + validation

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,11 +1,34 @@
+var User = require('../models/User');
+
 class UserController {
     async index(req, res) {
 
     }
 
     async create(req, res) {
-        console.log(req.body)
-        res.send('REQ OK')
+        var { name, email, password } = req.body
+
+        //Verificar se os campos foram preenchidos
+        if(email == undefined || name == undefined || password == undefined || password.length <= 0) {
+            res.status(400)
+            res.send('Preencha os campos corretamente')
+        }
+
+        //Verificar se o e-mail  já existe no banco de dados
+        var emailExists = await User.findEmail(email)
+
+        if(emailExists) {
+            res.status(401)
+            res.send('O e-mail já existe.')
+            return
+        }
+
+        //Criar o usuário
+        await User.new(name, email, password)
+
+        res.status(200)
+        res.send('Usuário cadastrado com sucesso')
+
     }
 }
 
---
+++ models/User.js
@@ -0,0 +1,35 @@
+const knex = require('../database/connection.js');
+const bcrypt = require('bcrypt');
+
+class User {
+
+    //Method responsável em realizar a criação do usuário
+    async new(name, email, password) {
+        try {
+            const hash = await bcrypt.hash(password, 10)
+            await knex.insert({name, email, password: hash, role: 0}).table('users')
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    //Method responsável em verificar se o e-mail já existe no banco de dados
+    async findEmail(email) {
+        try {
+            var result = await knex.select('*').from('users').where({email: email})
+
+            if(result.length > 0) {
+                return true
+            } else {
+                return false
+            }
+
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+    }
+}
+
+
+module.exports = new User()
