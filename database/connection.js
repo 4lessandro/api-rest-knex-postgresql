8c646f7204289a6abf8efc830534f1be2089b378
ascenciodev <alessandroascencio@protonmail.com>
2020-12-28 12:36:04 -0300

    create controller

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
-    client: 'mysql2',
+    client: 'pg',
     connection: {
       host : '127.0.0.1',
-      user : 'root',
-      password : '',
-      database : 'usertest'
+      user : 'postgres',
+      password : '123456',
+      database : 'users'
     }
   });
 
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var bodyParser = require('body-parser')
-var express = require("express")
+var express = require('express')
 var app = express()
-var router = require("./routes/routes")
+var router = require('./routes/routes')
  
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use("/",router);
+app.use('/', router);
 
 app.listen(80,() => {
     console.log("Servidor[OK] //sem erros")
